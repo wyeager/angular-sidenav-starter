3c5363a269cb4370e72873e4ef48b828cc9f4903
Will Yeager <yeager.will@gmail.com>
2019-04-23 20:26:11 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatSidenavModule, MatListModule, MatInputModule, MatButtonModule, MatSelectModule, MatIconModule, MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrier';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule, 
+    MatListModule, 
+    MatInputModule, 
+    MatButtonModule, 
+    MatSelectModule, 
+    MatIconModule, 
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
